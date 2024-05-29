112b1a09c953c803755821cc42742ab29f5c4f0f
umer-shehzad <umer.work.96@gmail.com>
2024-05-29 18:42:45 +0500

    separate projects data logic

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -6,64 +6,10 @@ import { BsArrowUpRight } from 'react-icons/bs';
 // variant
 import { fadeIn } from '../variants';
 
-import OqvicFlood from '../assets/oqvic-flood.png';
-import ExerciseTracker from '../assets/exercise-tracker.png';
-import EcommerceDashboard from '../assets/ecommerce-dashboard.png';
-import LittleLemon from '../assets/little-lemon.png';
-import TMS from '../assets/task-management-system.png';
-import GlobalGuide from '../assets/global-guide.png';
-import HumsafarPortfolio from '../assets/humsafar-portfolio.png';
+import { projects } from '../constants/ProjectsData';
 
 const Projects = () => {
     const [currentPage, setCurrentPage] = useState(0);
-
-    const projects = [
-        {
-            img: GlobalGuide,
-            pretitle: 'NEXT APP',
-            title: 'Global Guide',
-            liveLink: 'https://global-guide-next.vercel.app/',
-        },
-        {
-            img: HumsafarPortfolio,
-            pretitle: 'HTML CSS',
-            title: 'Humsafar Landing Page',
-            githubLink: 'https://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@github.com/app-humsafar/landingPage.git',
-            liveLink: 'https://humsafar-portfolio.vercel.app/',
-        },
-        {
-            img: TMS,
-            pretitle: 'REACT APP',
-            title: 'Task Management System',
-            liveLink: 'https://task-managemment-system-react.vercel.app/',
-        },
-        {
-            img: LittleLemon,
-            pretitle: 'REACT APP',
-            title: 'Little Lemon Restaurant',
-            githubLink: 'https://github.com/umer-shehzad/Little-Lemon-Restaurant-React',
-            liveLink: 'https://little-lemon-restaurant-react.vercel.app/',
-        },
-        {
-            img: OqvicFlood,
-            pretitle: 'REACT APP',
-            title: 'Oqvic Flood',
-            githubLink: 'https://github.com/umer-shehzad/Oqvic-Flood-App',
-            liveLink: 'https://oqvic-flood.web.app/',
-        },
-        {
-            img: ExerciseTracker,
-            pretitle: 'MERN APP',
-            title: 'Exercise Tracker',
-            githubLink: 'https://github.com/umer-shehzad/Exercise-Tracker-MERN-APP',
-        },
-        {
-            img: EcommerceDashboard,
-            pretitle: 'MERN APP',
-            title: 'Ecommerce Dashboard',
-            githubLink: 'https://github.com/umer-shehzad/e-commerce-dashboard-MERN',
-        },
-    ];
 
     const handlePrev = () => {
         if (currentPage > 0) {
---
+++ src/constants/ProjectsData.jsx
@@ -0,0 +1,56 @@
+import OqvicFlood from '../assets/oqvic-flood.png';
+import ExerciseTracker from '../assets/exercise-tracker.png';
+import EcommerceDashboard from '../assets/ecommerce-dashboard.png';
+import LittleLemon from '../assets/little-lemon.png';
+import TMS from '../assets/task-management-system.png';
+import GlobalGuide from '../assets/global-guide.png';
+import HumsafarPortfolio from '../assets/humsafar-portfolio.png';
+
+
+export const projects = [
+    {
+        img: GlobalGuide,
+        pretitle: 'NEXT APP',
+        title: 'Global Guide',
+        liveLink: 'https://global-guide-next.vercel.app/',
+    },
+    {
+        img: HumsafarPortfolio,
+        pretitle: 'HTML CSS',
+        title: 'Humsafar Landing Page',
+        githubLink: 'https://github.com/app-humsafar/landingPage',
+        liveLink: 'https://humsafar-portfolio.vercel.app/',
+    },
+    {
+        img: TMS,
+        pretitle: 'REACT APP',
+        title: 'Task Management System',
+        liveLink: 'https://task-managemment-system-react.vercel.app/',
+    },
+    {
+        img: LittleLemon,
+        pretitle: 'REACT APP',
+        title: 'Little Lemon Restaurant',
+        githubLink: 'https://github.com/umer-shehzad/Little-Lemon-Restaurant-React',
+        liveLink: 'https://little-lemon-restaurant-react.vercel.app/',
+    },
+    {
+        img: OqvicFlood,
+        pretitle: 'REACT APP',
+        title: 'Oqvic Flood',
+        githubLink: 'https://github.com/umer-shehzad/Oqvic-Flood-App',
+        liveLink: 'https://oqvic-flood.web.app/',
+    },
+    {
+        img: ExerciseTracker,
+        pretitle: 'MERN APP',
+        title: 'Exercise Tracker',
+        githubLink: 'https://github.com/umer-shehzad/Exercise-Tracker-MERN-APP',
+    },
+    {
+        img: EcommerceDashboard,
+        pretitle: 'MERN APP',
+        title: 'Ecommerce Dashboard',
+        githubLink: 'https://github.com/umer-shehzad/e-commerce-dashboard-MERN',
+    },
+];
